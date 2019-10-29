017693b8465f6629b03335bb09117346be1d6a49
LvPeng-Ubuntu <lvpeng990324@qq.com>
2019-10-29 21:19:03 +0800

    修改主路由，将活动报名app添加，并修改了招新app路由

--- submit/urls.py
+++ submit/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('submit/<name>/<class_num>/<phone>/<qq_num>/<department>/', views.submit),
-    path('', views.new_person_request),
+    path('new_person_request/', views.new_person_request),
     path('new_person_submit/', views.new_person_submit, name='new_person'),
     path('kexie_person_request/', views.kexie_person_request),
     path('kexie_person_submit/', views.kexie_person_submit, name='kexie_person'),
