6ba0cc62811424428fad28bda0ac86650468e471
LvPeng-Ubuntu <lvpeng990324@qq.com>
2019-10-30 14:41:47 +0800

    添加了django-cleanup组件，设置了media路径

--- kexie_online_submit/settings.py
+++ kexie_online_submit/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_cleanup',
     'submit',
     'activity_submit',
 ]
@@ -143,4 +144,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
