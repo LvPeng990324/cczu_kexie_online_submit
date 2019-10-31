24391962b27b72aad00a7c2627e641dc0cecb4ae
LvPeng-Ubuntu <lvpeng990324@qq.com>
2019-10-31 17:13:00 +0800

    处理了图片显示问题，做好了首页

--- activity_submit/urls.py
+++ activity_submit/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls.static import static
 from . import views
 
 
 urlpatterns = [
-    path('', views.test),
-]
+    path('', views.index, name='index'),
+
+    # 定义图片url
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- activity_submit/views.py
+++ activity_submit/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from.models import Activity, Student, SystemControl
 
-# Create your views here.
-def test(response):
-    pass
+
+# 活动报名入口
+def index(response):
+    if response.method == 'POST':
+        pass
+    else:
+        # 从数据库中取出活动信息并展示到主页
+        context = {
+            'name': Activity.objects.all()[0].name,
+            'introduction': Activity.objects.all()[0].introduction,
+            'qq_num': Activity.objects.all()[0].qq_num,
+            'qq_QRcode': Activity.objects.all()[0].qq_QRcode,
+        }
+        return render(response, 'index.html', context=context)
+
+
+# 报名表
+def submit_form(response):
+    if response.method == 'POST':
+        pass
+    else:
+        return redirect('/activity_submit/')
